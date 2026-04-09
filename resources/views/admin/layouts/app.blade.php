373edb27bfd3ba4176e975e1898c61773ee23ac0
workjaykanzariya-byte <work.jaykanzariya@gmail.com>
2026-04-09 17:27:20 +0530

    Revert "Unify admin sticky scroll behavior across circles and posts"
    This reverts commit 6728b71192dbac0cc827e600cde097fe699dd2a2.

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -234,6 +234,7 @@
 
                             if (node.matches?.(AREA_SELECTOR) || node.querySelector?.(AREA_SELECTOR)) {
                                 initAllAreas(node);
+                                return;
                             }
                         }
                     }
@@ -244,7 +245,6 @@
                 document.addEventListener('shown.bs.tab', () => window.AdminStickyScrollbar.refresh());
                 document.addEventListener('shown.bs.collapse', () => window.AdminStickyScrollbar.refresh());
                 document.addEventListener('shown.bs.modal', () => window.AdminStickyScrollbar.refresh());
-                window.addEventListener('load', () => window.AdminStickyScrollbar.refresh());
             });
         })();
     </script>
