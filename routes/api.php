1e0610e04d80a9fd23ede634f4b4537a26e50f41
Aequitas Developers <devops.aequitas@gmail.com>
2026-06-04 13:35:36 +0530

    Refactor authentication middleware structure in API routes

--- routes/api.php
+++ routes/api.php
@@ -133,9 +133,13 @@ Route::prefix('v1')->group(function () {
         Route::post('verify-otp', [AuthController::class, 'verifyOtp']);
         Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
         Route::post('reset-password', [AuthController::class, 'resetPassword']);
+
         Route::middleware('auth:sanctum')->group(function () {
 
+
+
         Route::middleware(['auth:sanctum', 'unity.user'])->group(function () {
+
             Route::post('logout', [AuthController::class, 'logout']);
             Route::get('me', [AuthController::class, 'me']);
         });
