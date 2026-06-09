f86e1b8e30c783d17e832ce9db69abaa2c8ddde3
workjaykanzariya-byte <work.jaykanzariya@gmail.com>
2026-06-09 15:55:59 +0530

    Improve coin claim submitted email

--- resources/views/emails/coin_claim_submitted.blade.php
+++ resources/views/emails/coin_claim_submitted.blade.php
@@ -1,3 +1,22 @@
-<p>Hello {{ $claim->user?->display_name ?? $claim->user?->first_name ?? 'Peer' }},</p>
-<p>Your coin claim has been submitted and is currently pending review.</p>
-<p>Activity: {{ $claim->activity_code }}</p>
+@component('mail::message')
+# Coin Claim Received
+
+Hello {{ $userName }},
+
+Thank you for submitting your coin claim. We have received your request and it is currently pending review by the Peers Global Unity team.
+
+## Claim Details
+
+| Detail | Information |
+| :--- | :--- |
+| Claim ID | {{ $claimId ?? '-' }} |
+| Activity | {{ $activityName ?? '-' }} |
+| Coins Claimed | {{ $coinsClaimed ?? '-' }} |
+| Submitted Date | {{ $submittedDate ?? '-' }} |
+| Status | {{ $status ?? 'Pending Review' }} |
+
+You will be notified once your claim has been reviewed.
+
+Thank you,<br>
+Peers Global Unity
+@endcomponent
