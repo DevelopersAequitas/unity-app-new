e62354f520345cce4273c69ddc05f41eea15892d
vinit chavda <devops.aequitas@gmail.com>
2026-07-18 13:05:35 +0530

    fix: align Reverb channels and Android FCM channel

--- config/firebase.php
+++ config/firebase.php
@@ -4,5 +4,5 @@ return [
     'project_id' => env('FIREBASE_PROJECT_ID'),
     'credentials_path' => env('FIREBASE_CREDENTIALS_PATH', env('FIREBASE_CREDENTIALS')),
     'credentials' => env('FIREBASE_CREDENTIALS_PATH', env('FIREBASE_CREDENTIALS')),
-    'default_android_channel_id' => env('FIREBASE_DEFAULT_ANDROID_CHANNEL_ID', 'default'),
+    'default_android_channel_id' => env('FIREBASE_DEFAULT_ANDROID_CHANNEL_ID', 'high_importance_channel'),
 ];
