f4141639bae351fa9cababb1ac31e80760b97782
Harsh Chauhan <devops.aequitas@gmail.com>
2026-07-29 20:07:24 +0530

    feat: implement support ticket email system and notification campaign service logic

--- app/Mail/SupportTicketResponseMail.php
+++ app/Mail/SupportTicketResponseMail.php
@@ -13,15 +13,34 @@ class SupportTicketResponseMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    /**
+     * @param  array<int, array{path: string, name: string, mime: string}>  $attachmentsList
+     */
     public function __construct(
         public SupportTicket $ticket,
         public string $emailSubject,
-        public string $responseMessage
+        public string $responseMessage,
+        public array $attachmentsList = []
     ) {}
 
     public function build(): self
     {
-        return $this->subject($this->emailSubject)
+        $mail = $this->subject($this->emailSubject)
             ->view('emails.support-ticket-response');
+
+        foreach ($this->attachmentsList as $attachment) {
+            if (isset($attachment['path']) && file_exists($attachment['path'])) {
+                $options = [];
+                if (! empty($attachment['name'])) {
+                    $options['as'] = $attachment['name'];
+                }
+                if (! empty($attachment['mime'])) {
+                    $options['mime'] = $attachment['mime'];
+                }
+                $mail->attach($attachment['path'], $options);
+            }
+        }
+
+        return $mail;
     }
 }
