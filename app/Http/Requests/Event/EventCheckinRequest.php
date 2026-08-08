f452c167ced31c86e90863d14b9726ae048f3d08
Vinit Chavda <devops.aequitas@gmail.com>
2026-08-08 16:27:32 +0530

    fix(events): map selected registration QR URL to WhatsApp media parameters

--- app/Http/Requests/Event/EventCheckinRequest.php
+++ app/Http/Requests/Event/EventCheckinRequest.php
@@ -8,7 +8,7 @@ class EventCheckinRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; 
+        return true;
     }
 
     public function rules(): array
